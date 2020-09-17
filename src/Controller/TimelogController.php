73702f837a0c58e42c2dc17de6c2d952f3a3a569
Probearbeit <probearbeit@SIOs-MacBook-Pro.local>
2020-09-17 11:08:47 +0200

    Added timelogs template

--- src/Controller/TimelogController.php
+++ src/Controller/TimelogController.php
@@ -14,8 +14,8 @@ class TimelogController extends AbstractController
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('timelogs.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
